0b4c32187f01e06bb526cd5ac9db4bfc6954bf7d
JohnSmith <jksa13@student.aau.dk>
2016-10-31 15:31:05 +0100

    lightlevel additions
    added checkpoints, changed checkpoint script a little, changed the
    rotation thing of light level,

--- SmartButler/Assets/Scripts/Character/Checkpoint.cs
+++ SmartButler/Assets/Scripts/Character/Checkpoint.cs
@@ -1,15 +1,18 @@
 ﻿using UnityEngine;
 using UnityStandardAssets.Characters.FirstPerson;
+using UnityStandardAssets.Characters.ThirdPerson;
 
 namespace Assets.Scripts.Character {
     public class Checkpoint : MonoBehaviour {
 
         private Despawner _despawner;
         private FirstPersonController FPScontroller;
+        private ThirdPersonCharacter TPScontroller;
 
         // Use this for initialization
         void Start() {
             FPScontroller = FindObjectOfType<FirstPersonController>();
+            TPScontroller = FindObjectOfType<ThirdPersonCharacter>();
             _despawner = GameObject.FindObjectOfType<Despawner>();
         }
 
@@ -21,6 +24,9 @@ namespace Assets.Scripts.Character {
         void OnTriggerEnter(Collider other) {
             if (other.gameObject.tag == "Player") {
                 FPScontroller.SetCurrentCheckpoint(this.transform.position);
+            }
+            if (other.gameObject.tag == "ThirdPerson") {
+                TPScontroller.SetCurrentCheckpoint(this.transform.position);
             }
         }
     }
