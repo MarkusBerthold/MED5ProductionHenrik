5b3cc1ee05802f1a34c61669f2eb80e18d6fddff
MarkusBerthold <mbert14@student.aau.dk>
2016-11-02 14:46:34 +0100

    Checks if an obstacle is between the player and camera

---
+++ SmartButler/Assets/Scripts/BetweenPlayerAndCamera.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using Assets.Scripts.Character;
+
+public class BetweenPlayerAndCamera : MonoBehaviour {
+
+    /// <summary>
+    /// Checks if the an obstacle is between the player and the camera. If so, then disable the renderer of that obstacle
+    /// </summary>
+    /// <param name="coll"></param>
+    void OnTriggerEnter(Collider coll)
+    {
+        if (coll.GetComponent<Despawner>() != null)
+        {
+            coll.GetComponent<Renderer>().enabled = false;
+        }
+    }
+}
