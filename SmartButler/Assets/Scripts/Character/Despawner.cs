1110bcb5c85671e03694d9edac508d64c6d88957
Holt95 <patrick1995holt@gmail.com>
2016-11-03 13:10:08 +0100

    Saves rotation
    The rotation is saved and reset on despawn. Can still risk falling
    endless because of checkpoint position.

--- SmartButler/Assets/Scripts/Character/Despawner.cs
+++ SmartButler/Assets/Scripts/Character/Despawner.cs
@@ -11,11 +11,13 @@ namespace Assets.Scripts.Character {
         public Vector3 Offset = Vector3.up; // is used for when a player is killed so that the player doesn't spawn inside the cog
         private FirstPersonController _player;
         private ThirdPersonCharacter _thirdPersonCharacter;
+        private LightRotation _lightRotation;
 
         //Initialise
         private void Start(){
             _player = FindObjectOfType<FirstPersonController>();
             _thirdPersonCharacter = FindObjectOfType<ThirdPersonCharacter>();
+            _lightRotation = FindObjectOfType<LightRotation>();
             //CurrentCheckpoint = GameObject.Find("RespawnPoint").transform;
             //_allFallingPlatforms = FindObjectsOfType<fallingPlatform>();
         }
@@ -48,6 +50,7 @@ namespace Assets.Scripts.Character {
                     break;
                 case "ThirdPerson":
                     _thirdPersonCharacter.RespawnChar();
+                    _lightRotation.ResetLevel();
                     break;
             }
 
