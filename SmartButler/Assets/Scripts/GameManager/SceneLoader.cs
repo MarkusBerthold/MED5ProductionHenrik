426ee57b56c6c8fde2ca4e5720b64519a2772f5b
DonEmil <emil@h58.aau1x.klient.sydhavn2.site.aau.dk>
2016-11-04 09:54:25 +0100

    Removed git auto merge lines

--- SmartButler/Assets/Scripts/GameManager/SceneLoader.cs
+++ SmartButler/Assets/Scripts/GameManager/SceneLoader.cs
@@ -4,81 +4,75 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-<<<<<<< HEAD
 namespace Assets.Scripts.GameManager {
 
 
-    /// <summary>
-    /// class for handling loading the four different scenes
-    /// </summary>
-    public class SceneLoader : MonoBehaviour {
-        public enum Scene {
-=======
-namespace Assets.Scripts.GameManager{
-    public class SceneLoader : MonoBehaviour{
-        public enum Scene{ //not sure how this works but it's used for switching between the scenes
->>>>>>> develop
-            LivingRoom,
-            Clock,
-            Stereo,
-            Light
-        }
+	/// <summary>
+	/// class for handling loading the four different scenes
+	/// </summary>
+	public class SceneLoader : MonoBehaviour {
+		public enum Scene {
+			LivingRoom,
+			Clock,
+			Stereo,
+			Light
+		}
 
-        private readonly float _distanceThreshold = 4.0f;
-        private bool _loading;
-        public GameObject Canvas;
-        public bool IsEnterable;
-        public Text LoadingText;
-        public Scene TargetScene;
+		private readonly float _distanceThreshold = 4.0f;
+		private bool _loading;
+		public GameObject Canvas;
+		public bool IsEnterable;
+		public Text LoadingText;
+		public Scene TargetScene;
 
-        void Start(){
-            _loading = false;
-        }
+		void Start(){
+			_loading = false;
+		}
 
-        private void OnMouseDown() {
-            var distanceToCam = Vector3.Distance(transform.position, Camera.main.gameObject.transform.position);
-            if (IsEnterable && (distanceToCam <= _distanceThreshold))
-                LoadScene();
+		private void OnMouseDown() {
+			var distanceToCam = Vector3.Distance(transform.position, Camera.main.gameObject.transform.position);
+			if (IsEnterable && (distanceToCam <= _distanceThreshold))
+				LoadScene();
 
-        }
+		}
 
-        //If player collides with this object, load a scene
-        private void OnTriggerEnter(Collider coll) {
-            if (coll.gameObject.tag == "Player" || coll.gameObject.tag == "ThirdPerson")
-                LoadScene();
+		//If player collides with this object, load a scene
+		private void OnTriggerEnter(Collider coll) {
+			if (coll.gameObject.tag == "Player" || coll.gameObject.tag == "ThirdPerson")
+				LoadScene();
 
-        }
+		}
 
-        /// <summary>
-        /// Loads a scene and shows "loading" text
-        /// </summary>
-        public void LoadScene() {
-            if (!_loading) {
-                // ...set the loadScene boolean to true to prevent loading a new scene more than once...
-                _loading = true;
-                Canvas.SetActive(true);
+		/// <summary>
+		///     Loads a scene and shows "loading" text
+		/// </summary>
+		public void LoadScene() {
+			if (!_loading) {
+				// ...set the loadScene boolean to true to prevent loading a new scene more than once...
+				_loading = true;
+				Canvas.SetActive(true);
 
-                // ...change the instruction text to read "Loading..."
-                LoadingText.text = "Level complete \n Loading...";
-                // ...and start a coroutine that will load the desired scene.
-                StartCoroutine(LoadNewScene());
-            }
-        }
+				// ...change the instruction text to read "Loading..."
+				LoadingText.text = "Level complete \n Loading...";
+				// ...and start a coroutine that will load the desired scene.
+				StartCoroutine(LoadNewScene());
+			}
+		}
 
 
-        private IEnumerator LoadNewScene() {
-            // Start an asynchronous operation to load the scene that was passed to the LoadNewScene coroutine.
-            //AsyncOperation async = Application.LoadLevelAsync(scene);
-            var async = SceneManager.LoadSceneAsync(TargetScene.ToString());
-            // While the asynchronous operation to load the new scene is not yet complete, continue waiting until it's done.
-            while (!async.isDone)
-                yield return null;
-            _loading = false;
-            Debug.Log("corutine ends");
-        }
+		private IEnumerator LoadNewScene() {
+			// Start an asynchronous operation to load the scene that was passed to the LoadNewScene coroutine.
+			//AsyncOperation async = Application.LoadLevelAsync(scene);
+			var async = SceneManager.LoadSceneAsync(TargetScene.ToString());
+			// While the asynchronous operation to load the new scene is not yet complete, continue waiting until it's done.
+			while (!async.isDone)
+				yield return null;
+			_loading = false;
+			Debug.Log("corutine ends");
+		}
 
 
-        /* private void OnLevelFinishedLoading(UnityEngine.SceneManagement.Scene arg0, LoadSceneMode arg1) {
+		/* private void OnLevelFinishedLoading(UnityEngine.SceneManagement.Scene arg0, LoadSceneMode arg1) {
              if (_loading) {
                  switch (TargetScene) {
                      case Scene.LivingRoom:
@@ -101,5 +95,5 @@ namespace Assets.Scripts.GameManager{
              }
          }*/
 
-    }
+	}
 }
