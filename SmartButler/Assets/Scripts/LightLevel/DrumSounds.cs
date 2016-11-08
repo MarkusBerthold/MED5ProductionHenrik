92576ce19f64277bd38a3b055ba97c9f0cc9b522
emil <emil.digit@gmail.com>
2016-11-08 11:39:28 +0100

    Random drums

--- SmartButler/Assets/Scripts/LightLevel/DrumSounds.cs
+++ SmartButler/Assets/Scripts/LightLevel/DrumSounds.cs
@@ -3,15 +3,35 @@ using System.Collections;
 
 public class DrumSounds : MonoBehaviour {
 
-    AudioClip [] audioclips
+	public AudioClip [] audioclips = new AudioClip[4];
+	public AudioClip organ;
+
+	private AudioSource DrumsAudioSource;
+	private AudioSource OrganAudioSource;
 
 	// Use this for initialization
 	void Start () {
 	
+		DrumsAudioSource = GameObject.FindGameObjectWithTag("drums").GetComponent<AudioSource>();
+		OrganAudioSource = GameObject.FindGameObjectWithTag("organ").GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+
+		if (!DrumsAudioSource.isPlaying) {
+
+			int rand = (int) (10f * Random.Range (0, 0.3f));
+			print (rand);
+			DrumsAudioSource.clip = audioclips [rand];
+			DrumsAudioSource.Play ();
+			OrganAudioSource.Play ();
+		}
+
+
+
+
 	
 	}
 }
