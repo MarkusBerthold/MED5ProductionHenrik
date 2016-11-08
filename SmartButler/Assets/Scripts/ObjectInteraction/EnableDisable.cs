bb21d301dfa6018b08ac1810d3409b1523d13ed7
MarkusBerthold <mbert14@student.aau.dk>
2016-11-08 09:47:12 +0100

    Added EventManager to EnableDisable

--- SmartButler/Assets/Scripts/ObjectInteraction/EnableDisable.cs
+++ SmartButler/Assets/Scripts/ObjectInteraction/EnableDisable.cs
@@ -2,35 +2,66 @@
 using System.Collections;
 using Assets.Scripts.Highlighting;
 using Assets.Scripts.ObjectInteraction;
+using UnityEngine.Events;
+using Assets.Scripts.MessageingSystem;
 
 public class EnableDisable : MonoBehaviour
 {
 
     private Component[] _highLighterComponents;
     private Component[] _lookAtTargetComponents;
-   
-	// Use this for initialization
 
+    private UnityAction _someListener;
 
-        /// <summary>
-        /// Disables all HighLighter and LookAtTarget scripts on itself and its children
-        /// </summary>
-    void Start()
-    {
+    // Use this for initialization
 
-        if (this.tag == "RemoteController" || this.tag == "Speaker")
-        {
+    void Awake() {
+        _someListener = EnableRemote;
+
+    }
+
+    void OnEnable() {
+
+        EventManager.StartListening("coffeebutton", _someListener);
+        EventManager.StartListening("remotecontrol", EnableSpeaker);
+
+    }
+
+    void OnDisable() {
+        EventManager.StopListening("coffeebutton", _someListener);
+        EventManager.StopListening("remotecontrol", EnableSpeaker);
+    }
+
+    /// <summary>
+    /// Enables all HighLighter and LookAtTarget scripts on itself and its children
+    /// </summary>
+    void EnableRemote() {
+        if (this.tag == "RemoteController") {
             _highLighterComponents = GetComponentsInChildren<Highlighter>();
             _lookAtTargetComponents = GetComponentsInChildren<LookAtTargets>();
-            for (int i = 0; i < _highLighterComponents.Length; i++)
-            {
-                _highLighterComponents[i].GetComponent<Highlighter>().enabled = false;
+            for (int i = 0; i < _highLighterComponents.Length; i++) {
+                _highLighterComponents[i].GetComponent<Highlighter>().enabled = true;
             }
 
-            for (int i = 0; i < _lookAtTargetComponents.Length; i++)
-            {
-                _lookAtTargetComponents[i].GetComponent<LookAtTargets>().enabled = false;
+            for (int i = 0; i < _lookAtTargetComponents.Length; i++) {
+                _lookAtTargetComponents[i].GetComponent<LookAtTargets>().enabled = true;
             }
         }
     }
+
+    void EnableSpeaker()
+    {
+        if (this.tag == "Speaker") {
+            _highLighterComponents = GetComponentsInChildren<Highlighter>();
+            _lookAtTargetComponents = GetComponentsInChildren<LookAtTargets>();
+            for (int i = 0; i < _highLighterComponents.Length; i++) {
+                _highLighterComponents[i].GetComponent<Highlighter>().enabled = true;
+            }
+
+            for (int i = 0; i < _lookAtTargetComponents.Length; i++) {
+                _lookAtTargetComponents[i].GetComponent<LookAtTargets>().enabled = true;
+            }
+        }
+    }
+
 }
