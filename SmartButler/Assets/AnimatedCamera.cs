fdc4540c5db54886d84127ea7240fecea168defe
Mporsi <atrixia@gmail.com>
2016-11-11 21:29:56 +0100

    Waking Animation works

---
+++ SmartButler/Assets/AnimatedCamera.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimatedCamera : MonoBehaviour {
+    private Animator _animator;
+    public GameObject FpsGameObject;
+
+    void Start(){
+        _animator = GetComponent<Animator>();
+    }
+
+	// Update is called once per frame
+	void Update () {
+	    if (_animator.GetCurrentAnimatorStateInfo(0).IsName("wakingCam")){
+	        
+	    }
+
+	}
+
+    public IEnumerator Destroy(){
+        FpsGameObject.SetActive(true);
+        yield return  new WaitForSeconds(1);
+        Destroy(gameObject);
+    }
+}
--- SmartButler/Assets/Scripts/Highlighting/Highlighter.cs
+++ SmartButler/Assets/Scripts/Highlighting/Highlighter.cs
@@ -4,24 +4,15 @@ namespace Assets.Scripts.Highlighting {
     public class Highlighter : MonoBehaviour {
         private GameObject Player;
         public float DistanceThreshold = 0.0f;
-        private bool _isWithinDistance;
-
-        void Start () {
-            Player = GameObject.FindGameObjectWithTag("Player");
-        }
-
-        void Update () {
-            if (Vector3.Distance(transform.position, Player.transform.position) < DistanceThreshold) {
-                _isWithinDistance = true;
-            } else {
-                _isWithinDistance = false;
-            }
-            //Debug.Log(OutlineShader.name);
-        }
+        private bool _isWithinDistance;     
 
         private void OnMouseOver() {
-            //print(Vector3.Distance(transform.position, Player.transform.position));
-            if (_isWithinDistance) {
+            if (!Player){
+                Player = GameObject.FindGameObjectWithTag("Player");
+                if (!Player)
+                    return;
+            }
+            if (Vector3.Distance(transform.position, Player.transform.position) < DistanceThreshold) {
                 gameObject.layer = 8;
             } else {
                 gameObject.layer = 0;
