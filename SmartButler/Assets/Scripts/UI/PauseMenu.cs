9329dedcfe135b511ecda5c6d8eaa96997c548e8
Mporsi <Atrixia@gmail.com>
2016-11-13 21:46:14 +0100

    PauseMenu done and done (#121)

---
+++ SmartButler/Assets/Scripts/UI/PauseMenu.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseMenu : MonoBehaviour {
+    public GameObject UI;
+    private GameObject PauseMenuPrefab;
+
+    void Awake(){
+        PauseMenuPrefab = Resources.Load("PauseMenuPrefab") as GameObject;
+    }
+
+    void Start(){
+        if (!(UI = GameObject.FindWithTag("PauseMenu")))
+            UI = Instantiate(PauseMenuPrefab);
+        TogglePauseMenu();
+    }
+
+    void Update(){
+        if (Input.GetKeyDown("escape"))
+            TogglePauseMenu();
+    }
+
+    public void TogglePauseMenu(){
+        // not the optimal way but for the sake of readability
+        if (UI.GetComponentInChildren<Canvas>().enabled){
+            UI.GetComponentInChildren<Canvas>().enabled = false;
+            Time.timeScale = 1.0f;
+        }
+        else{
+            UI.GetComponentInChildren<Canvas>().enabled = true;
+            Time.timeScale = 0f;
+        }
+    }
+
+
+    public void OnExit(){
+        AutoFade.LoadLevel(0, 3, 1, Color.black);
+    }
+}
