efbe0ae3d4a49ed4bb8be3711362cae380b48e47
Mporsi <Atrixia@gmail.com>
2016-11-13 20:25:13 +0100

    StartMenu And Rolling credits (#119)
    * StartMenu functionality done 
    
    needs art
    
    * Start Menu done

---
+++ SmartButler/Assets/Scripts/UI/ScrollingText.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using Assets.Scripts.MessageingSystem;
+
+public class ScrollingText : MonoBehaviour {
+
+    Vector2 _originalPosition;
+
+    void OnEnable()
+    {
+        _originalPosition = transform.position;
+    }
+
+    void OnDisable() {
+        transform.position = _originalPosition;
+    }
+
+    void Update() {
+        transform.Translate(Vector2.up);
+    }
+}
---
+++ SmartButler/Assets/Scripts/UI/StartMenu.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartMenu : MonoBehaviour {
+    public GameObject MenuPanel;
+    public GameObject CreditsPanel;
+
+    private bool _isViewingCredits = false;
+
+    public void OnCredits(){
+
+        _isViewingCredits = !_isViewingCredits;
+        MenuPanel.SetActive(!_isViewingCredits);
+        CreditsPanel.SetActive(_isViewingCredits);
+    }
+
+    public void OnExit(){
+        Application.Quit();
+    }
+
+    public void OnStartGame(){
+        AutoFade.LoadLevel("LivingRoom", 2, 1, Color.black);
+    }
+}
