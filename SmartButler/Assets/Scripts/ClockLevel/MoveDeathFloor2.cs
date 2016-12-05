02c250f250e890d45cbb9b6740ad08d67203baf1
JohnSmith <jksa13@student.aau.dk>
2016-12-05 15:00:30 +0100

    changed various things in clock-level

---
+++ SmartButler/Assets/Scripts/ClockLevel/MoveDeathFloor2.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveDeathFloor2 : MonoBehaviour {
+
+    public GameObject DeathFloor;
+
+    void OnTriggerEnter(Collider target) {
+        if (target.gameObject.tag == "Player") {
+            DeathFloor.transform.position = new Vector3(0, 17, 0);
+            Debug.Log("DeathFloor has been moved to: " + DeathFloor.transform.position);
+        }
+    }
+}
