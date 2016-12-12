6cf8dab2c39da464534aa9ddfe0c0e9702f80533
MarkusBerthold <mbert14@student.aau.dk>
2016-12-12 13:14:34 +0100

    Added wallClock broken script

---
+++ SmartButler/Assets/Scripts/ClockItem/fuckItUp.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using Assets.Scripts.ClockLevel;
+
+namespace Assets.Scripts.ClockItem
+{
+    public class fuckItUp : MonoBehaviour
+    {
+        private Component[] wallClockGearsZ;
+        private Component[] wallClockGearsY;
+
+        public GameObject wallClock;
+
+        // Use this for initialization
+        void Start()
+        {
+
+            
+
+        }
+
+        // Update is called once per frame
+        void OnMouseDown()
+        {
+
+            print("fucked it up1");
+
+            if (this.tag == "Speaker")
+            {
+
+                wallClockGearsZ = wallClock.GetComponentsInChildren<gearRotationZaxis>();
+                wallClockGearsY = wallClock.GetComponentsInChildren<GearRotationYAxis>();
+
+                print("fucked it up2");
+                for (int i = 0; i < wallClockGearsZ.Length; i++)
+                {
+                    wallClockGearsZ[i].GetComponent<gearRotationZaxis>().Rotspeed =
+                        wallClockGearsZ[i].GetComponent<gearRotationZaxis>().Rotspeed*10;
+                    
+                }
+
+                for (int i = 0; i < wallClockGearsY.Length; i++)
+                {
+                    wallClockGearsY[i].GetComponent<GearRotationYAxis>().Rotspeed =
+                        wallClockGearsY[i].GetComponent<GearRotationYAxis>().Rotspeed*10;
+                }
+            }
+        }
+    }
+}
